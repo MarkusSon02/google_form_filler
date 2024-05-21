fd87f10ce097d9db6a4ef9eade2a8b68877714fb
iamkdao <115352151+iamkdao@users.noreply.github.com>
2024-05-21 16:16:12 +0700

    Add files via upload

--- form_filler/components/functions.py
+++ form_filler/components/functions.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import random
 import string
+from components.constants import EXCEL_FILE
+from components.name_generator import Name_Generator
 
 def create_excel(entity_count, excel_file):
     data = []
+    name_gen = Name_Generator()
+    name_list = name_gen.generate()
     # Create some data as a DataFrame
     for i in range(entity_count):
         row = { 
-            "Name": random_string(),
+            "Name": name_list[i],
             "Age": random.randint(1, 100),
             "Gender": random.choice(["Male", "Female"]),
             "Option": random.randint(0,4)
@@ -17,7 +21,3 @@ def create_excel(entity_count, excel_file):
 
     # Write the DataFrame to an Excel file
     df.to_excel(excel_file, index=False)
-
-def random_string():
-    characters = string.ascii_letters
-    return ''.join(random.choice(characters) for i in range(random.randint(3, 10)))
--- form_filler/run.py
+++ form_filler/run.py
@@ -54,5 +54,3 @@ if __name__ == "__main__":
 
     for thread in threads:
         thread.join()
-
-    
